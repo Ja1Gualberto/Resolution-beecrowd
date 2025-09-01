a9a04dcf28af5d184b5569527db130a76e7d5412
João Pedro Gualberto Silva <164966121+Ja1Gualberto@users.noreply.github.com>
2025-09-01 15:14:18 -0400

    Create 1930.c

---
+++ 1930.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int T1, T2, T3, T4, R;
+    scanf("%i %i %i %i ", &T1, &T2, &T3, &T4);
+    R = T1 + T2 + T3 + T4 - 3;
+    printf("%i\n", R);
+ 
+    return 0;
+}
